6abebc17f1ce33997bf8c1f5cf5d007ab56dffa6
Sahil Sharma <sahilwhiteday@gmail.com>
2018-03-29 03:23:57 +0000

    delimandu project

---
+++ app.js
@@ -0,0 +1,20 @@
+var express= require('express');
+var app=   express();
+var bodyParser= require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine','ejs');
+// mongoose.connect("mongodb://localhost/ruz");
+
+
+
+// Routes
+
+app.get('/',function(req,res){
+    res.render('index.ejs');
+});
+
+
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log("Server has started!!!");
+})
